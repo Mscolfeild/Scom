cffa30f96d40cdc3bd47728d0cbb23e5a72f8332
Mustafa Mehmet YILMAZ <m.mehmetyilmaz46@gmail.com>
2022-08-14 00:49:25 +0300

    Add files via upload

---
+++ ServerMemberGroups.ps1
@@ -0,0 +1,38 @@
+﻿
+<#
+
+        Lists the groups that the server is a member of.
+        Change : $Server 
+
+#>
+
+$Group = @(Get-SCOMGroup)
+
+
+$Server = "Enter Server Name"
+
+
+
+
+
+
+for($m=0;$m -lt $Group.Count; $m++)
+{
+
+   $gr=  $Group[$m].GetRelatedMonitoringObjects()
+
+        for($i=0;$i -lt $gr.Count; $i++)
+        {
+ 
+          if($Server -eq $gr[$i] )
+          {
+                
+                $Group[$m]
+          }
+
+    
+   
+     }
+
+
+}
