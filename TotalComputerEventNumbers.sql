5fd54531b74467fe283b50107506b8dcb3c2eb1e
Mustafa Mehmet YILMAZ <m.mehmetyilmaz46@gmail.com>
2022-08-14 01:24:33 +0300

    Add files via upload

---
+++ TotalComputerEventNumbers.sql
@@ -0,0 +1,8 @@
+-- DB : OperationsManagerDW
+--It collects and lists the number of events that occurred between certain dates by server.
+--Change EventNumber
+SELECT evc.ComputerName,count(evc.ComputerName) as Total FROM Event.vEvent ev
+inner join EventLoggingComputer evc on ev.LoggingComputerRowId = evc.EventLoggingComputerRowId
+
+where ev.EventDisplayNumber in (x,y,...) and ev.DateTime >='2022-06-06 00:00:00' and ev.DateTime <='2022-06-08 23:59:00'
+group by evc.ComputerName, ev.EventNumber
