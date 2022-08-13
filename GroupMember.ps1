e1dedd5a2f39623838d3c643c8ff4e9c6768ba49
Mustafa Mehmet YILMAZ <m.mehmetyilmaz46@gmail.com>
2022-08-14 00:13:36 +0300

    Add files via upload

---
+++ GroupMember.ps1
@@ -0,0 +1,21 @@
+﻿
+<#
+
+    This script lists group members
+    
+    Change : GroupName and Path 
+#>
+
+Import-Module -Name OperationsManager
+
+$Group = Get-SCOMGroup |  where {$_.DisplayName -eq "GroupName"}
+
+$Path = XX/x.txt
+
+$Members = $Group.GetRelatedMonitoringObjects()
+
+$Members | Sort DisplayName | FT DisplayName
+
+$DisplayName = $Members |select DisplayName |Export-Csv $Path -NoTypeInformation
+
+
