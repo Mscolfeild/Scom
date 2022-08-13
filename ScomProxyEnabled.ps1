a9e198cf1386554957b4347aa139c89f91d2681c
Mustafa Mehmet YILMAZ <m.mehmetyilmaz46@gmail.com>
2022-08-13 19:37:57 +0300

    Add files via upload

---
+++ ScomProxyEnabled.ps1
@@ -0,0 +1,24 @@
+﻿<#
+
+Description:
+
+        Scom automatic agent proxy and remove alert operation console.
+
+        
+Creating a task scheduler on the server, it automatically deletes Agent proxy not enabled warnings.
+
+
+#>
+
+
+
+Import-Module -Name OperationsManager
+
+get-SCOMagent | where {$_.ProxyingEnabled -match "False"} | Enable-SCOMAgentProxy
+
+sleep 120 
+
+
+Get-SCOMAlert | Where-Object{($_.Name -eq "Agent proxy not enabled" -and ($_.ResolutionState -ne 255))} | Resolve-SCOMAlert
+
+
